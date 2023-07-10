c720ed08096167a69054904d6fc5ff2e1b5f1b02
BvRanjith <138455335+BvRanjith@users.noreply.github.com>
2023-07-10 14:29:47 +0530

    Update Linear search.cpp

--- Linear search.cpp
+++ Linear search.cpp
@@ -1,41 +1,26 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="Linear search" />
-		<Option execution_dir="C:/Users/Admin/Desktop/ai114/Linear search/" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/Linear search" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/Linear search" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-			<Add option="-fexceptions" />
-		</Compiler>
-		<Unit filename="C:/Users/Admin/Desktop/ai114/Linear search/main.cpp" />
-		<Extensions>
-			<lib_finder disable_auto="1" />
-		</Extensions>
-	</Project>
-</CodeBlocks_project_file>
+#include<iostream>
+using namespace std;
+
+void LinearSearch(int arr[], int len, int item){
+
+    for(int i=0;i<len;i++){
+        if(arr[i] == item){
+            cout << item << " Found at index : " << i;
+            return;
+        }
+    }
+    cout << "Not found";
+
+}
+
+int main() {
+    int arr[] = {10, 5, 15, 21, -3, 7};
+
+    int len = sizeof(arr)/sizeof(arr[0]);
+
+    int item = 21;
+    LinearSearch(arr, len, item);
+
+    return 0;
+}
+
